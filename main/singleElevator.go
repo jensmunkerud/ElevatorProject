71d286c45287a4b8aeb0ed4768ccb2e9c2ab1e3b
Jens <34352934+jensmunkerud@users.noreply.github.com>
2026-02-13 10:15:52 +0100

    implemented driver.go

--- main/singleElevator.go
+++ main/singleElevator.go
@@ -1,9 +1,6 @@
-package singeElevator
+package SingeElevator
 
-import (
-	"fmt"
-	"Driver-go"
-)
+import "Driver-go/elevio"
 
 type Behaviour string
 
@@ -31,4 +28,24 @@ type State struct {
 type Payload struct {
 	HallRequests [][]bool         `json:"hallRequests"`
 	States       map[string]State `json:"states"`
+}
+
+func InitSingleElevator(numFloors int) {
+
+	elevio.Init("localhost:15657", numFloors)
+
+	var d elevio.MotorDirection = elevio.MD_Up
+	//elevio.SetMotorDirection(d)
+
+	drv_buttons := make(chan elevio.ButtonEvent)
+	drv_floors := make(chan int)
+	drv_obstr := make(chan bool)
+	drv_stop := make(chan bool)
+
+	go elevio.PollButtons(drv_buttons)
+	go elevio.PollFloorSensor(drv_floors)
+	go elevio.PollObstructionSwitch(drv_obstr)
+	go elevio.PollStopButton(drv_stop)
+
+	return drv_buttons, drv_floors, drv_obstr, drv_stop
 }
