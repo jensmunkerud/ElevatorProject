86ac4586a6dc945cd3856fa38b9e370edd8ddbd0
Your Name <your.email@example.com>
2026-02-13 12:14:42 +0100

    Continued work on elevatorstruct

--- internal/elevatorStruct/elevatorStruct.go
+++ internal/elevatorStruct/elevatorStruct.go
@@ -1,9 +1,22 @@
 package elevatorstruct
 
 type Elevator struct {
+	numFloors int
+	hallResquests [][]bool
 	ID       int
 	Behaviour string
 	Floor    int
 	Direction string
-	cabRequest []int
+	cabRequest []bool
+
+}
+
+func (e Elevator) initialize(numFloors int, ID int) Elevator {
+	e.numFloors = numFloors
+	e.ID = ID
+	e.hallResquests = make([][]bool, 2)
+	e.Behaviour = "idle"
+	// Check which floor it is in
+	// Read what direction it is moving
+	e.cabRequest = [][]bool{}
 }
