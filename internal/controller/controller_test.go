7955de177a415fd13f79a3cf16a62aa2bb6a5b5f
Jens <34352934+jensmunkerud@users.noreply.github.com>
2026-02-14 13:45:43 +0100

    made elevator go to floor

--- internal/controller/controller_test.go
+++ internal/controller/controller_test.go
@@ -1,20 +1,34 @@
 package controller
 
 import (
+	"driver-go/elevio"
 	"fmt"
 	"testing"
 )
 
 func TestController(t *testing.T) {
 	orderEvent, floorEvent, obstructionEvent, stopEvent := InitController(4)
-
+	myFloor := -1
 	for {
 		select {
 		case a := <-orderEvent:
 			fmt.Printf("%+v\n", a)
+			if myFloor < 0 || myFloor == a.Floor {
+				continue
+			} else if myFloor < a.Floor {
+				elevio.SetMotorDirection(elevio.MD_Up)
+			} else if myFloor > a.Floor {
+				elevio.SetMotorDirection(elevio.MD_Down)
+			} else {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			}
 
 		case a := <-floorEvent:
 			fmt.Printf("%+v\n", a)
+			if myFloor == a {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			}
+			myFloor = a
 
 		case a := <-obstructionEvent:
 			fmt.Printf("%+v\n", a)
