90ae39a59d93e4b8262ef1a35a1815c6934e8716
razlin500 <razlin500@gmail.com>
2026-02-14 14:09:25 +0100

    Small changes to elevatorStruct

--- internal/elevatorStruct/elevatorStruct.go
+++ internal/elevatorStruct/elevatorStruct.go
@@ -1,21 +1,23 @@
 package elevatorstruct
 
+const (
+	numFloors = 4
+	numButtons = 3
+)
+
 type Elevator struct {
-	numFloors int
-	hallResquests [][2]bool //[up, down]
-	ID       int
-	Behaviour string
-	Floor    int
+	HallRequests [numFloors][2]bool //[up, down]
+	id       int
+	behaviour string
+	floor    int
 	Direction string
 	cabRequest []bool
 
 }
 
-func (e *Elevator) initialize(numFloors int, ID int) {
-	e.numFloors = numFloors
-	e.ID = ID
-	e.hallResquests = make([][2]bool, numFloors) //[up, down] * numFloors
-	e.Behaviour = "idle"
+func (e *Elevator) initialize(id int) {
+	e.id = id //[up, down] * numFloors
+	e.behaviour = "idle"
 	// Check which floor it is in
 	// Read what direction it is moving
 	e.cabRequest = make([]bool, numFloors)
