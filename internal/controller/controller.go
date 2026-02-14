f2312da839b72798f431693fcb8e757f2f2baf73
Jens <34352934+jensmunkerud@users.noreply.github.com>
2026-02-14 14:16:42 +0100

    working on splitting s/h IO and controller logic

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,8 +1,51 @@
 package controller
 
-import "driver-go/elevio"
+import (
+	"driver-go/elevio"
+	"fmt"
+)
 
-func InitController(numFloors int) (
+func InitController() {
+	orderEvent, floorEvent, obstructionEvent, stopEvent := InitElevatorIO(4)
+	myFloor := -1
+	targetFloor := -1
+	for {
+		select {
+		case a := <-orderEvent:
+			fmt.Printf("%+v\n", a)
+			targetFloor = a.Floor
+			if myFloor < 0 || myFloor == a.Floor {
+				continue
+			} else if myFloor < targetFloor {
+				elevio.SetMotorDirection(elevio.MD_Up)
+			} else if myFloor > targetFloor {
+				elevio.SetMotorDirection(elevio.MD_Down)
+			}
+
+		case a := <-floorEvent:
+			fmt.Printf("%+v\n", a)
+			myFloor = a
+			if myFloor == targetFloor || targetFloor < 0 {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			} else if myFloor < targetFloor {
+				elevio.SetMotorDirection(elevio.MD_Up)
+			} else if myFloor > targetFloor {
+				elevio.SetMotorDirection(elevio.MD_Down)
+			} else {
+				elevio.SetMotorDirection(elevio.MD_Stop)
+			}
+
+		case a := <-obstructionEvent:
+			fmt.Printf("%+v\n", a)
+
+		case a := <-stopEvent:
+			fmt.Printf("%+v\n", a)
+		}
+	}
+}
+
+// Handles IO communication between software and hardware
+func InitElevatorIO(numFloors int) (
 	chan elevio.ButtonEvent,
 	chan int,
 	chan bool,
--- internal/controller/controller_test.go
+++ internal/controller/controller_test.go
@@ -1,46 +1,9 @@
 package controller
 
 import (
-	"driver-go/elevio"
-	"fmt"
 	"testing"
 )
 
 func TestController(t *testing.T) {
-	orderEvent, floorEvent, obstructionEvent, stopEvent := InitController(4)
-	myFloor := -1
-	targetFloor := -1
-	for {
-		select {
-		case a := <-orderEvent:
-			fmt.Printf("%+v\n", a)
-			targetFloor = a.Floor
-			if myFloor < 0 || myFloor == a.Floor {
-				continue
-			} else if myFloor < targetFloor {
-				elevio.SetMotorDirection(elevio.MD_Up)
-			} else if myFloor > targetFloor {
-				elevio.SetMotorDirection(elevio.MD_Down)
-			}
-
-		case a := <-floorEvent:
-			fmt.Printf("%+v\n", a)
-			myFloor = a
-			if myFloor == targetFloor || targetFloor < 0 {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-			} else if myFloor < targetFloor {
-				elevio.SetMotorDirection(elevio.MD_Up)
-			} else if myFloor > targetFloor {
-				elevio.SetMotorDirection(elevio.MD_Down)
-			} else {
-				elevio.SetMotorDirection(elevio.MD_Stop)
-			}
-
-		case a := <-obstructionEvent:
-			fmt.Printf("%+v\n", a)
-
-		case a := <-stopEvent:
-			fmt.Printf("%+v\n", a)
-		}
-	}
+	InitController()
 }
