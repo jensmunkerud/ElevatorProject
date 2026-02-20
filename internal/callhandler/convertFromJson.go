b36828f64ed55e79a2c43494543336bcf0f3f019
Your Name <your.email@example.com>
2026-02-20 10:30:41 +0100

    Finished convertFromJson

--- internal/callhandler/convertFromJson.go
+++ internal/callhandler/convertFromJson.go
@@ -1,26 +1,31 @@
 package callhandler
 
+import (
+	"elevatorproject/internal/config"
+    "elevatorproject/internal/elevatorStruct"
+	"encoding/json"
+)
 
-func ParseElevatorJson(jsonStr string) (map[string]ElevatorButtons, error) {
-    var rawData map[string][][]bool
-    
-    err := json.Unmarshal([]byte(jsonStr), &rawData)
-    if err != nil {
-        return nil, err
-    }
-    
-    result := make(map[string]ElevatorButtons)
-    
-    for elevatorID, floors := range rawData {
-        var buttons ElevatorButtons
-        for i, floor := range floors {
-            if i < NumFloors && len(floor) >= 2 {
-                buttons[i][0] = floor[0]
-                buttons[i][1] = floor[1]
-            }
-        }
-        result[elevatorID] = buttons
-    }
-    
-    return result, nil
+func ParseElevatorJson(jsonStr string) (map[string]elevatorstruct.ElevatorButtons, error) {
+	var rawData map[string][][]bool
+
+	err := json.Unmarshal([]byte(jsonStr), &rawData)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make(map[string]elevatorstruct.ElevatorButtons)
+
+	for elevatorID, floors := range rawData {
+		var buttons elevatorstruct.ElevatorButtons
+		for floorNum, buttonDir := range floors {
+			if floorNum < config.NumFloors && len(buttonDir) >= 2 {
+				buttons.Buttons[floorNum][0] = buttonDir[0]
+				buttons.Buttons[floorNum][1] = buttonDir[1]
+			}
+		}
+		result[elevatorID] = buttons
+	}
+
+	return result, nil
 }
