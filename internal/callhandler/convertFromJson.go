5a5ad889b9712cf281c77e430fecdd31a804c6b6
Your Name <your.email@example.com>
2026-03-06 15:04:49 +0100

    Some commenting on convertFromJson

--- internal/callhandler/convertFromJson.go
+++ internal/callhandler/convertFromJson.go
@@ -6,6 +6,10 @@ import (
 	"encoding/json"
 )
 
+//A function that converts the JSON output from the cost function into a map of elevator IDs.
+//Each elevator ID maps to a struct containing the button states for that elevator. The JSON is expected to be in the format:
+//ID1: [[upButtonState, downButtonState], [upButtonState, downButtonState], ...],
+//ID2: [[upButtonState, downButtonState], [upButtonState, downButtonState], ...], ...
 
 func ConvertFromJson(jsonStr string) (map[string]elevatorstruct.ElevatorButtons, error) {
 	var rawData map[string][][]bool
