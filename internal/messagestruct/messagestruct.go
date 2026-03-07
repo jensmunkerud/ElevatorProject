6c3f127e3b67d34b5141f55e2297fed3dacdd833
razlin500 <razlin500@gmail.com>
2026-03-07 11:44:28 +0100

    Did some thinking

--- internal/messagestruct/messagestruct.go
+++ internal/messagestruct/messagestruct.go
@@ -1,14 +1,33 @@
 package messagestruct
 
 import  (
-	es "elevatorproject/internal/elevatorStruct"
-	os "elevatorproject/internal/orderStruct"
+	es "elevatorproject/internal/elevatorstruct"
 )
 
 type Message struct {
-	elev1 es.Elevator
-	elev2 es.Elevator
-	elev3 es.Elevator
-
-	orders os.Orders
+	elevators map[string]es.Elevator
 }
+
+// Adds a new elevator to the map of elevators in the message struct. 
+// If the elevator already exists, State Machine!!
+
+// handleElevatorStates()
+// handleOrderStates(){
+//     handleCabOrders()
+//     handleHallOrders()
+// }
+
+
+
+// t = 0
+// elev1.HallRequests = [unknown, unconfirmed]
+// elev2.HallRequests = [unknown, unknown]
+
+// t = 1
+// elev1.HallRequests = [unknown, unconfirmed]
+// elev2.HallRequests = [unknown, unconfirmed]
+
+// t = 2
+// elev1.HallRequests = [unknown, confirmed]
+// elev2.HallRequests = [unknown, confirmed]
+
