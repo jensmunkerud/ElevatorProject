d3e48e3c41dcb73ba4409d9d386d3cbed7922ae1
Your Name <your.email@example.com>
2026-03-13 13:50:43 +0100

    Added comments to help implementation of main and display moduleinteraction.

--- main.go
+++ main.go
@@ -3,9 +3,25 @@ package main
 
 
 func main() {
-	// Lage channels
-	// go initialize(lagdeChannels)	
+	// Initialize config, get my id, then lock to read only.
+	// Lage channels for communication between goroutines:
 
-	// go callHandlerInitialize(HallOrderUpdate, CabOrderUpdate, ElevatorState)
-	// go ElevatorServer(HallOrderUpdate, CabOrderUpdate, ElevatorState)
+	// HallOrderUpdate := make(chan orders.HallOrders)
+	// CabOrderUpdate := make(chan map[string]orders.CabOrders)
+	// ElevatorStateLocal := make(chan elevator.Elevator)
+	// CurrentOrders := make(chan elevatorserver.CallHandlerMessage)
+
+	// PeerUpdate := make(chan []string) MAYBE?
+	// WorldviewToCostFunction:= make(chan elevatorserver.OrderDistributorMessage)
+	// SendWorldviewToNetwork:= make(chan elevatorserver.NetworkingDistributorMessage)
+	// ReceiveWorldviewFromNetwork := make(chan elevatorserver.NetworkingDistributorMessage)
+
+	// ActiveOrders := make(chan [][]bool)
+
+	// Start goroutines:
+	// go controller(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal)
+	// go callHandler(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal, CurrentOrders)
+	// go ElevatorServer(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal, WorldviewToCostFunction, SendWorldviewToNetwork, ReceiveWorldviewFromNetwork, CurrentOrders)
+	// go RunNetworking(SendWorldviewToNetwork, ReceiveWorldviewFromNetwork, PeerUpdate)
+	// go costFunction(WorldviewToCostFunction, ActiveOrders)
 }
