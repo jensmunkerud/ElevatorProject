13dc8318b87b70982e56d5109750acbb33a3b65b
Your Name <your.email@example.com>
2026-03-14 16:09:15 +0100

    Almost finished main, missing controller and callhandler.

--- main.go
+++ main.go
@@ -4,28 +4,30 @@ import (
 	"elevatorproject/src/config"
 	"elevatorproject/src/elevator"
 	"elevatorproject/src/elevatorserver"
+	"elevatorproject/src/networking"
+	"elevatorproject/src/orderdistributor"
 )
 
 func main() {
 	//Set my ID before starting any goroutines.
 	config.SetMyID()
 	// Initialize channels for communication between goroutines:
-	hallOrderUpdate := make(chan elevatorserver.HallOrderUpdate, config.NumFloors*10) // Buffered to avoid blocking on sends
-	cabOrderUpdate := make(chan elevatorserver.CabOrderUpdate, config.NumFloors*10)
-	elevatorStateLocal := make(chan elevator.Elevator, config.NumFloors*4)
-	currentOrders := make(chan elevatorserver.CallHandlerMessage, config.NumFloors*10)
+	HallOrderUpdate := make(chan elevatorserver.HallOrderUpdate, config.NumFloors*10) // Buffered to avoid blocking on sends
+	CabOrderUpdate := make(chan elevatorserver.CabOrderUpdate, config.NumFloors*10)
+	ElevatorStateUpdate := make(chan elevator.Elevator, config.NumFloors*4)
+	
 	PeerUpdate := make(chan []string, 10) // Buffered to avoid blocking on sends
 
+	CurrentOrdersToCallhandler := make(chan elevatorserver.CallHandlerMessage, config.NumFloors*10)
 	WorldviewToOrderDistributor:= make(chan elevatorserver.OrderDistributorMessage, 5)
 	SendWorldviewToNetwork:= make(chan elevatorserver.NetworkingDistributorMessage, 5)
 	ReceiveWorldviewFromNetwork := make(chan elevatorserver.NetworkingDistributorMessage, 5)
-
-	ActiveOrders := make(chan [][]bool)
+	ActiveLocalOrders := make(chan [][]bool)
 
 	// Start goroutines:
 	// go controller(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal)
 	// go callHandler(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal, CurrentOrders)
-	// go ElevatorServer(HallOrderUpdate, CabOrderUpdate, ElevatorStateLocal, WorldviewToCostFunction, SendWorldviewToNetwork, ReceiveWorldviewFromNetwork, CurrentOrders)
-	// go RunNetworking(SendWorldviewToNetwork, PeerUpdate, ReceiveWorldviewFromNetwork)
-	// go costFunction(WorldviewToCostFunction, ActiveOrders)
+	go elevatorserver.RunElevatorServer(HallOrderUpdate, CabOrderUpdate, ElevatorStateUpdate, PeerUpdate, CurrentOrdersToCallhandler, WorldviewToOrderDistributor, SendWorldviewToNetwork, ReceiveWorldviewFromNetwork)
+	go networking.RunNetworking(SendWorldviewToNetwork, PeerUpdate, ReceiveWorldviewFromNetwork)
+	go orderdistributor.RunCostFunc(WorldviewToOrderDistributor, ActiveLocalOrders)
 }
